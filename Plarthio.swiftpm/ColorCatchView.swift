e10bd81fa945af23a6b03f3d10516dcfb4427acb
Rachit Bhatia <rachit.bhatia@configura.com>
2024-02-23 16:34:02 +0800

    shift ColorCatchHandler functions into ColorCatchView

--- Plarthio.swiftpm/ColorCatchView.swift
+++ Plarthio.swiftpm/ColorCatchView.swift
@@ -3,12 +3,16 @@ import SwiftUI
 
 struct ColorCatchView: View {
     private let collectorWidth: CGFloat = 100, collectorHeight: CGFloat = 100
-    private var colorCatchHandler: ColorCatchHandler
     private let rotationDuration: Double = 3
+    private let fallDistance: CGFloat = 320
+    
+    static var detectedHandPose: String = ""
     
     //color tray rotation
     @State private var isRotatingClockwise = true
-    @State private var trayRotationAngle: CGFloat = 0
+    @State private var updatedRotationAngle: CGFloat = 0
+    @State private var currentRotationAngle: CGFloat = 0
+    @State private var trayAngleScheduler: Timer?
     
     //coin rotation
     @State private var coinRotation: CGFloat = 0
@@ -17,7 +21,7 @@ struct ColorCatchView: View {
     @State private var coinOpacity: Double = 1
     private let coinFallDuration: Double = 3
     
-    //color tray
+    //color tray stored attributes
     private let colorArray = [Color.blue, Color.purple, Color.orange, Color.red]
     private let colorAngleDict: [Color:Double]
     
@@ -32,12 +36,12 @@ struct ColorCatchView: View {
                                colorArray[1]: 270.0,
                                colorArray[2]: 90.0,
                                colorArray[3]: 180.0]
-        self.colorCatchHandler = ColorCatchHandler(initColorArray: colorArray, initColorAngleDict: colorAngleDict)
     }
+
 
     var body: some View{
         ZStack {
-            Color.gray
+//            Color.gray
             VStack {
                 
                 Spacer()
@@ -55,11 +59,11 @@ struct ColorCatchView: View {
                 
                 //coin
                 Circle()
-                    .fill(self.currentCoinColor) 
+                    .fill(currentCoinColor) 
                     .stroke(Color.black, lineWidth: 3)
                     .frame(width: collectorWidth-20, height: collectorHeight-20)
                     .rotation3DEffect(.degrees(coinRotation), axis: (x: 0, y:1, z:0))
-                    .opacity(self.coinOpacity)
+                    .opacity(coinOpacity)
                     .offset(y: coinYOffset)
                     .zIndex(1)
                     .onAppear{
@@ -68,12 +72,11 @@ struct ColorCatchView: View {
                         }
                         
                         withAnimation(.linear(duration: coinFallDuration).repeatForever(autoreverses: false)) {
-                            self.coinYOffset = 320
+                            self.coinYOffset = fallDistance
                         }
                         
                         Timer.scheduledTimer(withTimeInterval: coinFallDuration, repeats: true) { scoreScheduler in
-                            
-                            let isCorrectColorCaught = self.colorCatchHandler.checkIfScored(self.currentCoinColor)
+                            let isCorrectColorCaught = self.checkIfScored(self.currentCoinColor)
                             
                             //change score based on color catch
                             if isCorrectColorCaught {
@@ -111,7 +114,7 @@ struct ColorCatchView: View {
                     }
                 
                 Spacer()
-                Spacer()
+                    .frame(height: fallDistance - 100)
                 
                 //color tray
                 ZStack {
@@ -142,12 +145,37 @@ struct ColorCatchView: View {
                             .frame(width: collectorWidth, height: collectorHeight)
                     }
                 }
-                .rotationEffect(.degrees(self.trayRotationAngle))
+                .rotationEffect(.degrees(self.updatedRotationAngle))
                 .onAppear {
-                   self.colorCatchHandler.recordRotationAngle(self.isRotatingClockwise)
+                    self.recordRotationAngle(self.isRotatingClockwise)
                    withAnimation(.linear(duration: rotationDuration).repeatForever(autoreverses: false)) {
-                      self.trayRotationAngle = 360
+                      self.updatedRotationAngle = 360
                    }
+                }
+                .onChange(of: ColorCatchView.detectedHandPose, initial: false) {
+                    
+                    //run action only if hand pose and current rotation direction do not correspond 
+                    if (ColorCatchView.detectedHandPose == "open_palm" && !isRotatingClockwise) || 
+                       (ColorCatchView.detectedHandPose == "closed_fist" && isRotatingClockwise) {
+                        
+                        self.stopRecordingRotationAngle()
+                        withAnimation(.linear(duration: 0)) {
+                            self.updatedRotationAngle = self.currentRotationAngle
+                        }
+                        
+                        let fullAngle: CGFloat
+                        if self.isRotatingClockwise {
+                            fullAngle = self.updatedRotationAngle - 360
+                        } else {
+                            fullAngle = self.updatedRotationAngle + 360
+                        }
+                        
+                        withAnimation(.linear(duration: rotationDuration).repeatForever(autoreverses: false)) {
+                            self.updatedRotationAngle = fullAngle
+                        }
+                        self.isRotatingClockwise.toggle()
+                        self.recordRotationAngle(self.isRotatingClockwise)
+                    }
                 }
                 
                 Spacer()
@@ -161,27 +189,26 @@ struct ColorCatchView: View {
                     .cornerRadius(20)
                     .animation(.linear(duration: 0), value: self.gameScore)
 
-                
                 Spacer()
                 
                 Button(action: {
-                    self.colorCatchHandler.stopRecordingRotationAngle()
+                    self.stopRecordingRotationAngle()
                     withAnimation(.linear(duration: 0)) {
-                        self.trayRotationAngle = self.colorCatchHandler.getCurrentAngle()
+                        self.updatedRotationAngle = self.currentRotationAngle
                     }
-                    
+                     
                     let fullAngle: CGFloat
                     if self.isRotatingClockwise {
-                        fullAngle = self.trayRotationAngle - 360
+                        fullAngle = self.updatedRotationAngle - 360
                     } else {
-                        fullAngle = self.trayRotationAngle + 360
+                        fullAngle = self.updatedRotationAngle + 360
                     }
                     
                     withAnimation(.linear(duration: rotationDuration).repeatForever(autoreverses: false)) {
-                        self.trayRotationAngle = fullAngle
+                        self.updatedRotationAngle = fullAngle
                     }
                     self.isRotatingClockwise.toggle()
-                    self.colorCatchHandler.recordRotationAngle(self.isRotatingClockwise)
+                    self.recordRotationAngle(self.isRotatingClockwise)
                 }) {
                     Text("Start")
                         .bold()
@@ -194,6 +221,49 @@ struct ColorCatchView: View {
                 }
                 Spacer()
             }
+        }
+    }
+    
+    //keeps a record of the angle in rotation through a timer
+    func recordRotationAngle(_ isClockwise: Bool) {
+        //increment angle every 0.01 seconds
+        trayAngleScheduler = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { _ in
+            if isClockwise {
+                currentRotationAngle += 1.2  //360 deg in 3s -> 1.2 deg in 0.01s
+            } else {
+                currentRotationAngle -= 1.2
+            }
+        }
+        trayAngleScheduler?.fire()
+    } 
+    
+    
+    func stopRecordingRotationAngle() {
+        self.trayAngleScheduler?.invalidate()
+        self.trayAngleScheduler = nil
+    }
+    
+    
+    //check if correct color is caught in the tray
+    func checkIfScored(_ currentCoinColor: Color) -> Bool {
+        var factor = Int(self.currentRotationAngle/360)
+        if self.currentRotationAngle < 0 && factor == 0 {
+            factor = -1  //convert angles between -360 and 0 to positive
+        }
+        
+        //convert angle to be in the range 0-360
+        let currentNormalisedAngle: CGFloat = self.currentRotationAngle - Double(factor*360)
+        
+        var currentCoinColorPosition = self.colorAngleDict[currentCoinColor]! + currentNormalisedAngle
+        if currentCoinColorPosition > 360 {
+            currentCoinColorPosition -= 360
+        }
+        
+        //check if coin is caught in the -40 to 40 degree range
+        if (currentCoinColorPosition >= 0 && currentCoinColorPosition <= 40) || (currentCoinColorPosition >= 320 && currentCoinColorPosition <= 360) {
+            return true
+        } else {
+            return false
         }
     }
 }
